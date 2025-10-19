5a3591eb4323a5178adbca487d17b23ba88a424d
Moaya0 <moayad543x@gmail.com>
2025-10-19 18:32:10 +0300

    Create LibrarySimulator.java

---
+++ LibrarySimulator.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Library {
+	public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+     
+
+//first user data
+int x1=045637122;//ID Number
+String x2=("Ahmed");//User name
+int x3=2;//Books Currently Borrowed
+
+
+//second user data
+int y1=045333333;//ID Number
+String y2=("Murad");//User name
+int y3=5;//Books Currently Borrowed
+
+
+
+
+//third user data
+int z1=045400254;//ID Number
+String z2=("Yasser");//User name
+int z3=10;//Books Currently Borrowed
+
+//Administrator
+
+
+
+System.out.println("Welcome to Library");
+System.out.println("Select a user account");
+System.out.println("Login as Administrator");
+System.out.println("Exit the program");
+String login=input.nextLine();
+if(login.equalsIgnoreCase("Select a user account")){
+	
+}
+else if (login.equalsIgnoreCase("Login as Administrator")) {
+System.out.print(w);
+}
+else if(login.equalsIgnoreCase("Exit the program")) {
+	
+}
+else
+	System.out.println("Error,please select again in the menu");
+	
+	}}
