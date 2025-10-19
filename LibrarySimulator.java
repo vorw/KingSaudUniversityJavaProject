f5a53924dd8b5bfc9b1fbc4ec930a94effd617cd
salsanie <65559313+vorw@users.noreply.github.com>
2025-10-19 20:42:03 +0300

    User Data Handling in LibrarySimulator

--- LibrarySimulator.java
+++ LibrarySimulator.java
@@ -1,47 +1,89 @@
 import java.util.Scanner;
 public class Library {
-	public static void main(String[] args) {
-     Scanner input = new Scanner(System.in);
-     
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-//first user data
-int x1=045637122;//ID Number
-String x2=("Ahmed");//User name
-int x3=2;//Books Currently Borrowed
+        // First User Data
+        int id_1 = 045637122; // ID Number
+        String username_1 = ("Ahmed"); // User name
+        int firstBorrowed = 2; // Books Currently Borrowed
+        int firstBorrowedThisSession = 0; // Books Borrowed This Session
 
+        // Second User Data
+        int id_2 = 0453333333; // ID Number
+        String username_2 = ("Murad"); // User name
+        int secondBorrowed = 5; // Books Currently Borrowed
+        int secondBorrowedThisSession = 0; // Books Borrowed This Session
 
-//second user data
-int y1=045333333;//ID Number
-String y2=("Murad");//User name
-int y3=5;//Books Currently Borrowed
+        // Third User Data
+        int id_3 = 045400254; // ID Number
+        String username_3 = ("Yasser"); // User name
+        int thirdBorrowed = 10; // Books Currently Borrowed
+        int thirdBorrowedThisSession = 0; // Books Borrowed This Session
 
-
-
-
-//third user data
-int z1=045400254;//ID Number
-String z2=("Yasser");//User name
-int z3=10;//Books Currently Borrowed
-
-//Administrator
+        // Administrator
 
 
 
-System.out.println("Welcome to Library");
-System.out.println("Select a user account");
-System.out.println("Login as Administrator");
-System.out.println("Exit the program");
-String login=input.nextLine();
-if(login.equalsIgnoreCase("Select a user account")){
-	
+
+        System.out.println("The Library Welcomes You!");
+        System.out.println("----------------------")
+        System.out.println("1 - Select A User Account");
+        System.out.println("2 - Login As Administrator");
+        System.out.println("3 - Exit");
+        String menu = input.nextLine();
+        if (menu == "1") {
+            System.out.println("Available Users:");
+            System.out.println("1 - " + username_1 + " ID: " + id_1);
+            System.out.println("2 - " + username_2 + " ID: " + id_2);
+            System.out.println("3 - " + username_3 + " ID: " + id_3);
+            String user = input.nextLine();
+            if (user == "1") {
+                System.out.println("Look Who's Back, " + username_1 + " Glad To See You Again!");
+                System.out.println("1 - View Borrowed Books");
+                System.out.println("2 - Borrow Book");
+                System.out.println("3 - Return Book");
+                System.out.println("4 - View Session Summary");
+                System.out.println("5 - Exit");
+                String function = input.nextLine();
+                if (function == "1") {
+                    System.out.println("You Currently Have " + firstBorrowed + " Books Borrowed.");
+                if (function == "2") {
+                    System.out.println("How Many Books?");
+                    int numberOfBooks = input.nextInt();
+            } else if (user == "2") {
+                System.out.println("Look Who's Back, " + username_2 + " Glad To See You Again!");
+                System.out.println("1 - View Borrowed Books");
+                System.out.println("2 - Borrow Book");
+                System.out.println("3 - Return Book");
+                System.out.println("4 - View Session Summary");
+                System.out.println("5 - Exit");
+                String function = input.nextLine();
+                if (function == "1") {
+                    System.out.println("You Currently Have " + secondBorrowed + " Books Borrowed.");
+                } else if (function == "2") {
+                    System.out.println("How Many Books?");
+                    int numberOfBooks = input.nextInt();
+            } else if (user == "3") {
+                System.out.println("Look Who's Back, " + username_3 + " Glad To See You Again!");
+                System.out.println("1 - View Borrowed Books");
+                System.out.println("2 - Borrow Book");
+                System.out.println("3 - Return Book");
+                System.out.println("4 - View Session Summary");
+                System.out.println("5 - Exit");
+                String function = input.nextLine();
+                if (function == "1") {
+                    System.out.println("You Currently Have " + thirdBorrowed + " Books Borrowed.");
+                } else if (function == "2") {
+                    System.out.println("How Many Books?");
+                    int numberOfBooks = input.nextInt();
+            } else
+                System.out.println("You've only got 3 numbers..");
+        } else if (menu == "2")) {
+            System.out.print(w);
+        } else if (menu == "3") {
+
+        } else
+            System.out.println("You've only got 3 numbers..");
+    }
 }
-else if (login.equalsIgnoreCase("Login as Administrator")) {
-System.out.print(w);
-}
-else if(login.equalsIgnoreCase("Exit the program")) {
-	
-}
-else
-	System.out.println("Error,please select again in the menu");
-	
-	}}
