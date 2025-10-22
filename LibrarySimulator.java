a34d05e4fc50f1b6822dc5db27b67471587e3cab
salsanie <65559313+vorw@users.noreply.github.com>
2025-10-22 22:13:09 +0300

    Rename LibraryProject to LibrarySimulator

--- LibrarySimulator.java
+++ LibrarySimulator.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class LibraryProject {
+public class LibrarySimulator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
